3527b4ad0612ef1c87ea426a8db0f6e6bf0be18a
flycool <flycoolcool@163.com>
2023-08-11 19:41:14 +0800

    add bottomNavBar

--- app/src/main/java/com/compose/sample/composeui/dragfood/DragDropMainScreen.kt
+++ app/src/main/java/com/compose/sample/composeui/dragfood/DragDropMainScreen.kt
@@ -1,8 +1,5 @@
-package com.compose.sample.composeui
+package com.compose.sample.composeui.dragfood
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxScope
@@ -15,36 +12,46 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.compose.sample.composeui.dragfood.DragDropMainScreen
-import com.compose.sample.composeui.dragfood.FoodItemCard
-import com.compose.sample.composeui.dragfood.PersonCard
 import com.compose.sample.composeui.dragfood.function.LongPressDraggable
 import com.compose.sample.composeui.dragfood.model.FoodItem
-import com.compose.sample.composeui.dragfood.model.foodItems
 import com.compose.sample.composeui.dragfood.model.persons
-import com.compose.sample.composeui.ui.theme.ComposeuiTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ComposeuiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    BottomNavBarScreen()
-                }
+@Composable
+fun DragDropMainScreen(
+    list: List<FoodItem>
+) {
+    LongPressDraggable(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(horizontal = 10.dp)
+        ) {
+            items(list) { food ->
+                FoodItemCard(foodItem = food)
             }
         }
+        PersonListContainer()
     }
 }
 
+@Composable
+fun BoxScope.PersonListContainer() {
+    LazyRow(
+        modifier = Modifier
+            .fillMaxHeight(0.3f)
+            .fillMaxWidth()
+            .background(Color.LightGray, RoundedCornerShape(topEnd = 10.dp, topStart = 10.dp))
+            .padding(vertical = 10.dp)
+            .align(Alignment.BottomCenter),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        items(persons) { person ->
+            PersonCard(person = person)
+        }
+    }
+}
