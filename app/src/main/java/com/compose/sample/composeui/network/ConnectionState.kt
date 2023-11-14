e1c1bb3083d30608e67dab2fd90c9d85033910ed
flycool <flycoolcool@163.com>
2023-11-14 12:24:07 +0800

    add observe Network connectivity

---
+++ app/src/main/java/com/compose/sample/composeui/network/ConnectionState.kt
@@ -0,0 +1,97 @@
+package com.compose.sample.composeui.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+
+
+@Composable
+fun ConnectivityStatus() {
+    val connection by connectivityState()
+    val isConnected = connection == ConnectionState.Available
+    if (isConnected) {
+        Text(text = "Connect is available")
+    } else {
+        Text(text = "Connect is Unavailable", color = Color.Red)
+    }
+}
+
+@Composable
+fun connectivityState(): State<ConnectionState> {
+    val context = LocalContext.current
+    return produceState(initialValue = context.currentConnectivityState) {
+        context.observeConnectivityAsFlow().collect {
+            value = it
+        }
+    }
+}
+
+sealed class ConnectionState {
+    object Available : ConnectionState()
+    object Unavailable : ConnectionState()
+}
+
+val Context.currentConnectivityState: ConnectionState
+    get() {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return getCurrentConnectivityState(connectivityManager)
+    }
+
+private fun getCurrentConnectivityState(connectivityManager: ConnectivityManager): ConnectionState {
+    val connected = connectivityManager.allNetworks.any { network ->
+        connectivityManager.getNetworkCapabilities(network)
+            ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+    }
+
+    return if (connected) ConnectionState.Available else ConnectionState.Unavailable
+}
+
+
+fun Context.observeConnectivityAsFlow() = callbackFlow {
+    val connectivityManager =
+        getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    val callback = NetworkCallback { connectionState -> trySend(connectionState) }
+
+    val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .build()
+
+    connectivityManager.registerNetworkCallback(networkRequest, callback)
+
+    val currentState = getCurrentConnectivityState(connectivityManager)
+    trySend(currentState)
+
+    awaitClose {
+        connectivityManager.unregisterNetworkCallback(callback)
+    }
+}
+
+fun NetworkCallback(callback: (ConnectionState) -> Unit): ConnectivityManager.NetworkCallback {
+    return object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            callback(ConnectionState.Available)
+        }
+
+        override fun onUnavailable() {
+            callback(ConnectionState.Unavailable)
+        }
+
+
+        override fun onLost(network: Network) {
+            callback(ConnectionState.Unavailable)
+        }
+    }
+}
